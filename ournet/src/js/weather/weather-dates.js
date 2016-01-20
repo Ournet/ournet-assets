7c37afbab441839d1cc2e0504ef1de6c7da24882
Dumitru Cantea <canteadumitru@gmail.com>
2016-01-20 05:15:54 +0200

    install bootstrap by npm

--- ournet/src/js/weather/weather-dates.js
+++ ournet/src/js/weather/weather-dates.js
@@ -1,12 +1,12 @@
-﻿(function($, gd) {
-	"use strict";
+(function($, gd) {
+	'use strict';
 
-	$('#cn-summary').on('click', 'th', function() {
+	$('#w-summary-menu').on('click', 'li', function() {
 		var th = $(this);
 		$.get('/controls/main_places_weather/' + th.attr('data-date'), {
 			ul: gd.ul,
 		}, function(result) {
-			$.each($('#cn-summary th'), function() {
+			$.each($('#w-summary-menu li'), function() {
 				$(this).removeClass('selected');
 			});
 			th.addClass('selected');
