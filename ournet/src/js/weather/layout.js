d04273be57fb39e66a764c01c63b0b6f2cb90399
Dumitru Cantea <canteadumitru@gmail.com>
2016-02-21 14:21:14 +0200

    start initial simple ournet layout

--- ournet/src/js/weather/layout.js
+++ ournet/src/js/weather/layout.js
@@ -6,29 +6,6 @@
 		});
 	}
 
-	function scrollEnable() {
-		var currentScrollTop = 0;
-		var pastScrollTop = 0;
-
-		function onChangedScroll() {
-			currentScrollTop = $(d).scrollTop();
-			if (currentScrollTop === 0 || pastScrollTop === 0 && currentScrollTop >= 0 && currentScrollTop !== pastScrollTop) {
-				console.log('aici', currentScrollTop, pastScrollTop);
-				if (currentScrollTop > 0) {
-					$('#page-header').removeClass('trans');
-					$('#page-header').addClass('solid');
-				} else {
-					$('#page-header').removeClass('solid');
-					$('#page-header').addClass('trans');
-				}
-				pastScrollTop = currentScrollTop;
-			}
-		}
-		$(w).scroll(onChangedScroll);
-
-		onChangedScroll();
-	}
-
 	function affixEnable() {
 		$('#affix-right-region').affix({
 			offset: {
@@ -40,7 +17,6 @@
 
 	$(d).ready(function() {
 		searchEnable();
-		scrollEnable();
 		affixEnable();
 	});
 })(document, window);
