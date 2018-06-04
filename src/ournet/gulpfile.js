2051c9e338028518df912ef5931182913d96b40c
Dumitru Cantea <canteadumitru@gmail.com>
2018-06-04 17:26:17 +0300

    ournet folders structure

--- src/ournet/gulpfile.js
+++ src/ournet/gulpfile.js
@@ -8,6 +8,7 @@ const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const rev = require('gulp-rev-all');
+const connect = require('gulp-connect');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -25,10 +26,10 @@ gulp.task('img', ['imagemin']);
 
 const cssDist = '../../dest/ournet/css';
 
-gulp.task('sass', function () {
+gulp.task('sass', function () { 
     return gulp.src([
-        './scss/main.scss',
-    ])
+        './scss/weather/main.scss',
+    ], { base: './scss' })
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(cssDist))
         // .pipe(sourcemaps.init())
@@ -38,14 +39,21 @@ gulp.task('sass', function () {
         .pipe(gulpif(isProduction, rev.manifestFile()))
         // .pipe(sourcemaps.write())
         // .pipe(rename({ basename: config.css.main }))
-        .pipe(gulp.dest(cssDist));
+        .pipe(gulpif(isProduction, gulp.dest(cssDist)));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./scss/*.scss', ['sass']);
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('connect', function () {
+    connect.server({
+        root: '../../dest/ournet',
+        port: parseInt(process.env.PORT),
+    });
 });
 
 
-
 gulp.task('watch', ['sass:watch']);
+gulp.task('serv', ['watch', 'connect']);
 gulp.task('default', ['img', 'sass']);
